80211c7b49e2ab0201aa28183b56d6260554e00c
Meirivaldo <95186840+Meirivaldo@users.noreply.github.com>
2022-02-16 21:54:56 -0300

    comments

--- src/one/dio/com/Quadrilatero.java
+++ src/one/dio/com/Quadrilatero.java
@@ -2,6 +2,7 @@ package one.dio.com;
 
 
 /**
+ *
  * Classe de exemplo para o exercicio da Aula 3 de métodos.
  */
 public class Quadrilatero {
@@ -18,6 +19,24 @@ public class Quadrilatero {
         return ((baseMaior*baseMenor)*altura) / 2;
     }
 
+/*
+  O tipo de retorno tem que está compativel com a criação da sua definição, se não for compativel da erro
+  de compilação!
 
+  Ex.:
+  Não se pode pedir um retorno de um long para um double!
+
+  public static long exemplo1(){
+      return 1.6;
+  }
+
+  Para se corrigir o erro de compilação deve trocar o *metodo* long para double!
+
+  public static double exemplo2(){
+      return 1.6;
+    }
+   *
+
+ */
 
 }
